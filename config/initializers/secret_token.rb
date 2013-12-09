54a41949785304974e9dac3e1b2f15f91f3eace3
Jonathan Lake <jonlake5@gmail.com>
2013-12-08 21:18:52 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OutdoorJournal::Application.config.secret_key_base = 'f547d28f696205e9093ffbb891533e92e6bcafe62fc8a6c42e8123efbfa55f4bc3d4db5da39086e4aea81585c746fe4b3a6e906c8db9d868b7245fc7dfc4fa4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OutdoorJournal::Application.config.session_store :cookie_store, key: '_outdoor_journal_session'
