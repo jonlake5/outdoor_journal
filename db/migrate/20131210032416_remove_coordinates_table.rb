046442fdd1f4828661a30164e2e963c0ea200903
Jonathan Lake <jonlake5@gmail.com>
2013-12-10 22:44:08 -0500

    Update migrations

--- db/migrate/20131210032416_remove_coordinates_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCoordinatesTable < ActiveRecord::Migration
-  def change
-  drop_table :adventure_coordinates
-  end
-end
--- db/migrate/20131211033210_drop_adventure_coordinates_table.rb
+++
@@ -1,5 +0,0 @@
-class DropAdventureCoordinatesTable < ActiveRecord::Migration
-  def change
-    drop_table :adventure_coordinates
-  end
-end
