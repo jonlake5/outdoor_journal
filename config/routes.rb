29b372b7965923703bd357358a1bf4b70c4b8dc3
Jonathan Lake <jonlake5@gmail.com>
2013-12-14 21:48:01 -0500

    Created initial new user page and edited the user controller. Also added a route for /signup to point to the new user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 OutdoorJournal::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+
+
+
+  match '/signup', to ':users#new'via: 'get'
+
+
+
+
+
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
