3b77a1a86b2a78abf99542506864b3442bb45330
Jonathan Lake <jonlake5@gmail.com>
2013-12-16 07:22:40 -0500

    Sessions signin works.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216033723) do
+ActiveRecord::Schema.define(version: 20131216115418) do
 
   create_table "adventure_asset_types", force: true do |t|
     t.string   "adventure_asset_type"
@@ -77,8 +77,10 @@ ActiveRecord::Schema.define(version: 20131216033723) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
