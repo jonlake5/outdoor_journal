618270cc3cf7ddd6e5e857caf18f229b772bb3d3
Jonathan Lake <jonlake5@gmail.com>
2013-12-21 22:48:37 -0500

    Everything seems to be working up to the point of 9.3.2 in the demo app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
@@ -18,6 +23,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
 
   def destroy
@@ -26,11 +44,23 @@ class UsersController < ApplicationController
   end
 
   private
-  def params_user
-    params.require(:user).permit(:f_name, :l_name, :email,
-                                :password, :password_confirmation,                                 :address, :city, 
-                                :state, :zip)
+    def params_user
+      params.require(:user).permit(:f_name, :l_name, :email,
+                                  :password, :password_confirmation,
+                                  :address, :city, 
+                                  :state, :zip)
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in" unless signed_in?
+      end
     end
 
+    def correct_user
+      @user=User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 end
