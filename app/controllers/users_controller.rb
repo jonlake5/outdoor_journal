7af91d62686ade4290f53f25c036e49efa5a11ee
Jonathan Lake <jonlake5@gmail.com>
2013-12-23 22:28:28 -0500

    Corrected issue where any user could see all adventures

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update, :index]
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update, :show]
   before_action :admin_user,     only: [:destroy]
   def show
     @user = User.find(params[:id])
