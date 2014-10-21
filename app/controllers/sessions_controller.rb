4ab25c37df84048bb0e0fdb91c33b4e6cc089b60
Jonathan Lake <jonlake5@gmail.com>
2014-10-20 20:51:59 -0400

    Changed action after login to send to all adventures

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
    user = User.find_by(email: params[:session][:email].downcase)
    if user && user.authenticate(params[:session][:password])
     sign_in user
-    redirect_back_or user
+    #redirect_back_or user
+    redirect_to url_for(:controller => :adventures, :action => :index)
    else
      flash.now[:error] = 'Invalid email/password combination'
      render 'new'
