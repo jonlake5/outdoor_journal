b508b8941bcbde2647946e9a22008c628a14159e
Jonathan Lake <jonlake5@gmail.com>
2014-10-24 14:58:48 -0400

    Added confirmations to deleteing adventure and all coordinates. It still asks twice, but that might just be in development mode

--- app/assets/javascripts/adventures.js
+++ app/assets/javascripts/adventures.js
@@ -24,3 +24,16 @@ function initialize() {
 
   
 google.maps.event.addDomListener(window, 'load', initialize); 
+
+
+
+
+
+function checkField(){
+    var v = document.getElementById("coords_uploaded_file").value;
+    if(v == ''){
+      alert("Please enter a file name or use\nthe browse button to select a file.");
+      return false;
+    }
+    return true;
+};
--- app/assets/javascripts/upload_checker.js
+++
@@ -1,8 +0,0 @@
-function checkField(){
-    var v = document.getElementById("coords_uploaded_file").value;
-    if(v == ''){
-      alert("Please enter a file name or use\nthe browse button to select a file.");
-      return false;
-    }
-    return true;
-};
